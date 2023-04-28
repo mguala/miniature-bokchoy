3df275e0f65241e1d6ff57086683ee3320a1c420
Melissa Guala <mguala@users.noreply.github.com>
2023-04-27 22:31:47 -0400

    Update rut.py
    Change comment coding

--- rut.py
+++ rut.py
@@ -1,6 +1,6 @@
 rut = reversed(input('ingresa tu RUT::>')) #reversa el rut al input
 
-d = sum([int(n)*list(range(2,8))[i%6] for i,n in enumerate(rut)]) //inside out -> mueve el index del input a la vez que mueve el index del range 2,8
+d = sum([int(n)*list(range(2,8))[i%6] for i,n in enumerate(rut)]) #inside out -> mueve el index del input a la vez que mueve el index del range 2,8
 
 d %= 11 //divide por 11
 
