2efd9e6192366aed5e9cdae8b6ba54a86dd097a1
Melissa Guala <mguala@users.noreply.github.com>
2023-04-27 22:22:37 -0400

    Create rut.py

---
+++ rut.py
@@ -0,0 +1,12 @@
+rut = reversed(input('ingresa tu RUT::>')) #reversa el rut al input
+
+d = sum([int(n)*list(range(2,8))[i%6] for i,n in enumerate(rut)]) //inside out -> mueve el index del input a la vez que mueve el index del range 2,8
+
+d %= 11 //divide por 11
+
+if (d==1):
+    d = 'K'
+else:
+    d = 11-d
+    
+print(d)
